6d8b5971876d5a3cb019c1de77647c9a80c67b03
leonardom249 <leonardo.megan@gmail.com>
2018-06-05 13:28:29 -0500

    formatted graph and added view count to percentages

--- view_graph.py
+++ view_graph.py
@@ -5,11 +5,11 @@ from matplotlib.gridspec import GridSpec
 
 videos= youtube_grabber.videoInfo()
 
-vid1Title='First Place: ' + videos['Video1']['Title']
-vid2Title='Second Place: ' + videos['Video2']['Title']
-vid3Title='Third Place: ' + videos['Video3']['Title']
-vid4Title='Fourth Place: ' + videos['Video4']['Title']
-vid5Title='Fifth Place: ' + videos['Video5']['Title']
+vid1Title='First Place: \n' + videos['Video1']['Title']
+vid2Title='Second Place: \n' + videos['Video2']['Title']
+vid3Title='Third Place: \n' + videos['Video3']['Title']
+vid4Title='Fourth Place: \n' + videos['Video4']['Title']
+vid5Title='Fifth Place: \n' + videos['Video5']['Title']
 
 vid1Views=videos['Video1']['Views'].replace(',', '')
 vid2Views=videos['Video2']['Views'].replace(',', '')
@@ -20,44 +20,29 @@ vid5Views=videos['Video5']['Views'].replace(',', '')
 labels = vid1Title , vid2Title, vid3Title, vid4Title, vid5Title
 fracs = [int(vid1Views), int(vid2Views), int(vid3Views), int(vid4Views), int(vid5Views)]
 
+def make_autopct(fracValues):
+    def my_autopct(pct):
+        total = sum(fracValues)
+        val = int(round(pct*total/100.0))
+        return '{p:.2f}%  \n ({v:d})'.format(p=pct,v=val)
+    return my_autopct
 
 explode = (0.05, 0, 0, 0, 0)
 
-the_grid = GridSpec(2, 2)
-
-# plt.subplot(the_grid[0, 0], aspect=1)
-
-# plt.pie(fracs, labels=labels, autopct='%1.1f%%', shadow=True)
-
-# plt.subplot(the_grid[0, 0], aspect=1)
-
-# font = {'weight'   : 'bold'}
-
-# plt.pie(fracs, explode=explode, textprops=font, labels=labels, autopct='%.0f%%', shadow=True)
+the_grid = GridSpec(1, 1)
 
 plt.subplot(the_grid[0, 0], aspect=1)
 
 patches, texts, autotexts = plt.pie(fracs, explode=explode, labels=labels,
-                                    autopct='%.0f%%',
+                                    autopct=make_autopct(fracs),
                                     shadow=True, radius=1)
 
-# # Make the labels on the small plot easier to read.
 for t in texts:
     t.set_size('smaller')
+    t.set_multialignment('center')
 for t in autotexts:
     t.set_size('x-small')
-autotexts[0].set_color('y')
-
-# plt.subplot(the_grid[1, 1], aspect=1)
-
-# # Turn off shadow for tiny plot with exploded slice.
-# patches, texts, autotexts = plt.pie(fracs, explode=explode,
-#                                     labels=labels, autopct='%.0f%%',
-#                                     shadow=False, radius=0.5)
-# for t in texts:
-#     t.set_size('smaller')
-# for t in autotexts:
-#     t.set_size('x-small')
 # autotexts[0].set_color('y')
+
 
 plt.show()
