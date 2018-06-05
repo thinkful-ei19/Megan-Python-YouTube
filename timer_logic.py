d4aba36f1d42677e0a6eced044c9f4fd5124ca1b
leonardom249 <leonardo.megan@gmail.com>
2018-06-05 14:00:24 -0500

    have basic example of timer function down

---
+++ timer_logic.py
@@ -0,0 +1,20 @@
+from threading import Timer
+
+def twoArgs(arg1,arg2):
+    print(arg1)
+    print(arg2)
+
+def nArgs(*args):
+    for each in args:
+        print(each)
+
+#arguments: 
+#how long to wait (in seconds), 
+#what function to call, 
+#what gets passed in
+r = Timer(1.0, twoArgs, ("arg1","arg2"))
+s = Timer(2.0, nArgs, ("OWLS","OWLS","OWLS"))
+
+
+r.start()
+s.start()
