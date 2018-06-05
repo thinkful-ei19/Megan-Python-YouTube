02d77db46cf9b21f6256eef877b5143db983ee34
leonardom249 <leonardo.megan@gmail.com>
2018-06-05 12:18:56 -0500

    fixed original object to contain multiple video objects

--- youtube_grabber.py
+++ youtube_grabber.py
@@ -18,15 +18,17 @@ def videoInfo():
     count=0
     for videoComponent in findVideos:
         count= count+1
+        video=dict()
         titlePart = videoComponent.find(attrs={"class": "yt-lockup-title"})
         titleLink = titlePart.find("a")
         titleUrl = titleLink.get('href')
         titleName = titleLink.get('title')
         infoOnVideo = videoComponent.find(attrs={"class": "yt-lockup-meta-info"})
         viewCount = infoOnVideo.contents[1].string.split(" ")[0]
-        videoObj['Title{}'.format(count)]= titleName
-        videoObj['URL{}'.format(count)]= titleUrl
-        videoObj['Views{}'.format(count)]= viewCount
+        video['Title']= titleName
+        video['URL']= titleUrl
+        video['Views']= viewCount
+        videoObj['Video{}'.format(count)]=video
 
     return videoObj
 
